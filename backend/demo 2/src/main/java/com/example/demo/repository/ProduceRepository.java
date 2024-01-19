fb14a4cde5b053948da1ae7e2bb6704cfeefd3fb
naimaaaaaa <2134714@brunel.ac.uk>
2024-01-19 13:58:57 +0000

    Revert "Initial commit"
    This reverts commit dee82efc5e55e30d6ece3b16a08d1778c0ca1381.

--- backend/demo 2/src/main/java/com/example/demo/repository/ProduceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Produce;
-
-public interface ProduceRepository extends CrudRepository<Produce,Long>{
-
-}
